4fe2d4f32c9808f045b0a11fdb02f3dc03264a1d
GR <linux2.0@foxmail.com>
2019-12-26 16:45:25 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by hzlx on 2019/12/26.
+//  Copyright © 2019 GR. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

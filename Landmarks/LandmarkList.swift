0e1884a7a24c800983edb660d2ea4884f5b5f5ae
GR <linux2.0@foxmail.com>
2019-12-27 18:00:52 +0800

    Building Lists and Navigation

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,32 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by hzlx on 2019/12/27.
+//  Copyright © 2019 GR. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone 11 Pro"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}

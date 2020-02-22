bf3f80717d008e447befd54e1288aa0bc8f2de08
GR <linux2.0@foxmail.com>
2020-02-22 10:38:20 +0800

    Interfacing with UIKit

--- Landmarks/SceneDelegate.swift
+++ Landmarks/SceneDelegate.swift
@@ -18,14 +18,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        // Create the SwiftUI view that provides the window contents.
+//        let contentView = CategoryHome().environmentObject(UserData())
+        let contentView = PageView(features.map { FeatureCard(landmark: $0) })
+            .aspectRatio(3 / 2, contentMode: .fit)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(
-                rootView: CategoryHome()
-                    .environmentObject(UserData())
-            )
+            window.rootViewController = UIHostingController(rootView: contentView)
             self.window = window
             window.makeKeyAndVisible()
         }
